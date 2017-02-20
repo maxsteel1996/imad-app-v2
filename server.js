3922cb6ea85ed62bca26fb3aeef8c8e2422272ce
maxsteel1996 <yusufmax3@gmail.com>
2017-02-20 12:21:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:"Article one| Yusuf ",
+    heading:"Article one",
+    date:"20 feb 2017"
+    content:"<p>
+           This is my first article and i m writig some meaningless words so just stop and dont read it further, r u still reading ? huh ok keep wasting ur time u r not gonna find anything useful or which make sense but if u still keep reading, u r dumb as fuck... just stop u peace of shit... u r so stubborn u r not gonna stop right? ok fine i dont care :p
+        </p>
+        <p>
+            This is again some random words so just dont read it ok?
+        </p>
+        <p>
+            Agin blah blah
+        </p>"
+    
+};
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate="
+<html>
+    <head>
+        <title>$(title)</title>
+        <meta name="viewport" content="width-device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="container">
+        <div><a href="/">Home</a></div>
+        <hr>
+        <h3>$(heading)</h3>
+        <div>$(date)</div>
+        <div>
+        $(content)
+        </div>
+        </div>
+    </body>
+</html>";
+return htmlTemplate;
+    
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
